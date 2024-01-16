1bf46d76d8825f4082e5df1ba25f55e0508a844d
smallziz <abdoulaziz17.diouf@ucad.edu.sn>
2024-01-16 17:08:19 +0000

    pour si on aurra un domaiin lancer la verification des email

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Models\User;
+use http\Client\Request;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 use App\Models\Annonce;
 use App\Models\Image;
@@ -32,7 +34,20 @@ Route::middleware(['auth','verified'])->group(function () {
     Route::post('/userannonce', [AnnonceController::class, 'store'])->name('annonces.store');
 
 });
+Route::get('email/verify',function (){
+   return view('auth.verify-email');
+})->middleware('auth')->name('verification-notice');
 
+Route::get('/email/verify/{id}/{hash}',function (EmailVerificationRequest $request){
+    $request->fulfill();
+    return redirect('/profile');
+
+})->middleware(['auth','signed'])->name('verification.verify');
+Route::post('/email/verification-notification',function (\Illuminate\Http\Request $request){
+    $request->user()->sendEmailVerifiationNotification();
+    return back()->with('message','Verification link sent!');
+
+})->middleware(['auth','throttle:6.1'])->name('verification.send');
 
 Route::get('/annonce/{id}', [AnnonceController::class, 'show']);
 
