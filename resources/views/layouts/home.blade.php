730c2feaa6edb32e642427edeedcfaa0ff646b68
Cheikh TOURAD <elghothvadel@gmail.com>
2024-01-30 13:03:49 +0000

    page pour chaque annonces

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -78,6 +78,16 @@
 <nav id="header" class="w-full z-30 top-0 py-1">
     <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
 
+        <div class="order-1 md:order-2">
+            <a class="flex items-center tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="">
+                <svg class="fill-current text-gray-800 mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M5,22h14c1.103,0,2-0.897,2-2V9c0-0.553-0.447-1-1-1h-3V7c0-2.757-2.243-5-5-5S7,4.243,7,7v1H4C3.447,8,3,8.447,3,9v11 C3,21.103,3.897,22,5,22z M9,7c0-1.654,1.346-3,3-3s3,1.346,3,3v1H9V7z M5,10h2v2h2v-2h6v2h2v-2h2l0.002,10H5V10z" />
+                </svg>
+
+                Dakar Deals
+
+            </a>
+        </div>
         <label for="menu-toggle" class="cursor-pointer md:hidden block">
             <svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
                 <title>menu</title>
@@ -93,17 +103,6 @@
                     @livewire('bare-de-recherche')
                 </ul>
             </nav>
-        </div>
-
-        <div class="order-1 md:order-2">
-            <a class="flex items-center tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="">
-                <svg class="fill-current text-gray-800 mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path d="M5,22h14c1.103,0,2-0.897,2-2V9c0-0.553-0.447-1-1-1h-3V7c0-2.757-2.243-5-5-5S7,4.243,7,7v1H4C3.447,8,3,8.447,3,9v11 C3,21.103,3.897,22,5,22z M9,7c0-1.654,1.346-3,3-3s3,1.346,3,3v1H9V7z M5,10h2v2h2v-2h6v2h2v-2h2l0.002,10H5V10z" />
-                </svg>
-
-                Dakar Deals
-
-            </a>
         </div>
 
         <div class="order-2 md:order-3 flex items-center" id="nav-content">
@@ -129,10 +128,13 @@
                         <a href="{{ url('/profile') }}" >Mon profil</a>
                         </x-primary-button>
                     @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Se connecter</a>
-
+                        <x-primary-button>
+                        <a href="{{ route('login') }}" >Se connecter</a>
+                        </x-primary-button>
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">S'inscrire</a>
+                            <x-primary-button>
+                            <a href="{{ route('register') }}" >S'inscrire</a>
+                            </x-primary-button>
                         @endif
                     @endauth
                 </div>
