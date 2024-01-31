abac05fef2f1bd060977983f75b851da31d900a9
Cheikh TOURAD <elghothvadel@gmail.com>
2024-01-31 19:11:16 +0000

    page pour chaque annonces

---
+++ app/Models/Favoris.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Favoris extends Model
+{
+    use HasFactory;
+    public function annonce()
+    {
+        return $this->belongsTo(Annonce::class, 'annonce_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(Annonce::class, 'user_id');
+    }
+}
