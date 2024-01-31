e5f83fc2e20590bc6e2bd778b291c7fe0de22e20
Cheikh TOURAD <elghothvadel@gmail.com>
2024-01-31 17:52:02 +0000

    page pour chaque annonces

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::get('/annonce/{id}', [AnnonceController::class, 'show']);
 
 
 Route::get('/', function () {
-    $annonces = Annonce::with('images', 'user')->paginate(15);
+    $annonces = Annonce::with('images', 'user')->paginate(70);
     return view('welcome', compact('annonces'));
 })->name('welcome');
 
