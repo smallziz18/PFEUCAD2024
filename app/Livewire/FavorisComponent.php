1f0b7a5b08ff08b86da6d6b1fc07d7585eb947d5
Cheikh TOURAD <elghothvadel@gmail.com>
2024-02-04 13:33:48 +0000

    page pour chaque annonces

--- app/Livewire/FavorisComponent.php
+++ app/Livewire/FavorisComponent.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Favoris;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 use Livewire\Component;
 use App\Models\Annonce;
 
@@ -12,32 +13,37 @@ class FavorisComponent extends Component
 
 
 {
-    public function toggleFavori($annonceId): void
+    public function toggleFavori($annonceId)
     {
+        if (Auth::check()) {
 
-        $favorisExist = Favoris::where('user_id', Auth::id())
-            ->where('annonce_id', $annonceId)
-            ->exists();
 
-        if ($favorisExist) {
-            Favoris::where('user_id', Auth::id())
+            $favorisExist = Favoris::where('user_id', Auth::id())
                 ->where('annonce_id', $annonceId)
-                ->delete();
+                ->exists();
+
+            if ($favorisExist) {
+                Favoris::where('user_id', Auth::id())
+                    ->where('annonce_id', $annonceId)
+                    ->delete();
 
 
-            session()->flash('message', 'Annonce retirée des favoris avec succès.');
-            session()->flash('alert-class', 'alert-success');
+                session()->flash('message', 'Annonce retirée des favoris avec succès.');
+                session()->flash('alert-class', 'alert-success');
 
-        } else {
-            Favoris::create([
-                'user_id' => Auth::id(),
-                'annonce_id' => $annonceId,
-            ]);
+            } else {
+                Favoris::create([
+                    'user_id' => Auth::id(),
+                    'annonce_id' => $annonceId,
+                ]);
 
 
-
-            session()->flash('message', 'Annonce ajoutée aux favoris avec succès.');
-            session()->flash('alert-class', 'alert-success');
+                session()->flash('message', 'Annonce ajoutée aux favoris avec succès.');
+                session()->flash('alert-class', 'alert-success');
+            }
+        }
+        else{
+           Redirect::route('login');
         }
     }
 
