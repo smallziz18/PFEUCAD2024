fa2110ee2decb949212e75aa55640998c75a1d3d
Cheikh TOURAD <elghothvadel@gmail.com>
2024-02-07 17:14:53 +0000

    page pour chaque annonces

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -145,9 +145,9 @@
         </div>
     </div>
 </nav>
-<section class="bg-white py-8">
+<section class="bg-white py-8" >
 
-    <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
+    <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12" style="margin-top: 100px">
 
 
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -167,8 +167,8 @@
                 @endforeach
                     <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
                         <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-                    <h3 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Date :{{ $annonce->created_at->format('d/m/Y') }}</h3>
-                <h3 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Publié Par :{{$user->name}}</h3>
+                            <h3 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Date :{{ $annonce->created_at->format('d/m/Y') }}</h3>
+                            <h3 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Publié Par :{{$user->name}}</h3>
                             <a href="https://wa.me/{{ $user->telephone }}?text=Bonjour%20{{$user->name}}%20,%20je%20suis%20intéressé%20par%20votre%20annonce%20{{ $annonce->titre }}%20Pourriez-vous%20m'en%20dire%20plus%20?" target="_blank" rel="noopener noreferrer">
                                 <h3 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                                     Numéro : {{ $user->telephone }}
