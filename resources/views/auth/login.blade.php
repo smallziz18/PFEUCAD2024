b5debc0b2626538d95f2071c388722f0fe81b8dc
Cheikh TOURAD <elghothvadel@gmail.com>
2024-02-09 08:15:16 +0000

    page pour chaque annonces

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,6 +40,12 @@
                 Se connecter avec Google
             </a>
         </div>
+        <a href="/auth/facebook/redirect" class="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2">
+            <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+                <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
+            </svg>
+            Se connecter avec Facebook
+        </a>
 
         <!-- Remember Me -->
         <div class="block mt-4">
