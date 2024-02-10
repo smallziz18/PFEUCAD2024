5ba24da48814538777b0e86b3c8f845a472c32d6
Cheikh TOURAD <elghothvadel@gmail.com>
2024-02-10 19:34:10 +0000

    page pour chaque annonces

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -219,7 +219,7 @@
                 @endif
                 @if(Session::has('signal'))
                     <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-                        <strong class="font-bold">Succès!</strong>
+
                         <span class="block sm:inline">{{ Session::get('signal') }}</span>
                     </div>
                 @endif
