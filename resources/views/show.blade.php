d82d09721cb89ec26b69c376d470a8d5ac4a1f90
Cheikh TOURAD <elghothvadel@gmail.com>
2024-02-10 13:16:00 +0000

    page pour chaque annonces

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -192,7 +192,10 @@
                         </a>
                         <h3 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Nombre de like : {{$annonce->like}}</h3>
                         <h3 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Nombre de vue : {{$annonce->vue}}</h3>
-                        @if() @endif
+                        @if($annonce->livrable)
+
+                            <a href="#" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        @endif
 
 
 
