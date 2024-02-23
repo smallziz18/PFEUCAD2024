435d3314e8ba23a508b1318ce9f26f8c17e4ba27
Cheikh TOURAD <elghothvadel@gmail.com>
2024-02-23 13:43:53 +0000

    page pour chaque annonces

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -377,7 +377,7 @@ Alternatively if you want to just have a single hero
             <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16m0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15"/>
         </svg>
 
-        <p class="text-lg font-semibold">Découvrez nos annonces les plus populaires :</p>
+        <p class="text-lg text-blue-700 font-semibold">Découvrez nos annonces les plus populaires :</p>
     </div>
     <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
 
@@ -460,7 +460,7 @@ Alternatively if you want to just have a single hero
             <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
         </svg>
 
-        <p class="text-lg font-semibold">Découvrez nos annonces les plus recente :</p>
+        <p class="text-lg text-blue-700 font-semibold">Découvrez nos annonces les plus recente :</p>
     </div>
 
     <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
